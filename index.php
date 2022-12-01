a5d8e3e0cb9c01742874278d12b75f6515f4d890
AlexLugo94 <93061911+AlexLugo94@users.noreply.github.com>
2022-12-01 05:51:05 -0500

    menu en proceso

--- index.php
+++ index.php
@@ -4,9 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<link rel="stylesheet" href="styles/styles.css">  
     <title>Document</title>
 </head>
 <body>
-    
-</body>
+    <header>
+    <div class="collapse" id="navbarToggleExternalContent">
+  <div class="bg-dark p-4">
+    <h5 class="text-white h4">Collapsed content</h5>
+    <span class="text-muted">Toggleable via the navbar brand.</span>
+  </div>
+</div>
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
+</nav>
+</header>
+
+    <section class="seccionUno">
+        <h1 class="tituloUno">conoce acerca de nosotros, somos expertos en sistemas!!!</h1>
+        <br>
+        <br>
+        <p class="pa-uno">con nuestro sistema de gestion de tareas tendras todos los beneficios de un usuario premium</p>
+        <p class="pa-dos">con nuestro sistema de gestion de tareas tendras todos los beneficios de un usuario premium</p>
+          <p class="pa-tres">con nuestro sistema de gestion de tareas tendras todos los beneficios de un usuario premium</p>
+
+    </section>
+
+
+<footer>
+
+</footer>
+ </body>
 </html>
