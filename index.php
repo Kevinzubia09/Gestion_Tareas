5b3fdeef1f76437a2a5228bf01ea958da7dbb1b1
AlexLugo94 <93061911+AlexLugo94@users.noreply.github.com>
2022-12-04 21:11:53 -0500

    correcion de errores

--- index.php
+++ index.php
@@ -1,49 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-<link rel="stylesheet" href="styles/styles.css">  
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="styles/styles.css">
+
+    <title>gestion de tareas2</title>
 </head>
+
 <body>
     <header>
-      <nav>
-   <div class="nav justify-content-center">
-  <li class="nav-item">
-    <a class="nav-item" class="nav-link active" aria-current="page" href="login_tareas/index.php">Ingresa</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Registrate</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">entrar como visitante</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled">Conoce nuestras redes sociales</a>
-  </li>
-</div>
-</nav>
-</header>
+        <nav>
+            <div class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-item" class="nav-link active" aria-current="page"
+                        href="login_tareas/index.php">Ingresa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Registrate</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">entrar como visitante</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Conoce nuestras redes sociales</a>
+                </li>
+            </div>
+        </nav>
+    </header>
     <section class="seccionUno">
 
         <img src="img/ffoondo.jpg" alt="" class="ffoondo">
-        
+
         <h1 class="tituloUno">Conoce acerca de nosotros, somos expertos en sistemas.!!!</h1>
         <br>
         <br>
-        <p class="pa-uno">Con nuestro sistema de gestion de tareas tendras todos los beneficios de un usuario premium.</p>
+
+        <p class="pa-uno">Con nuestro sistema de gestion de tareas tendras todos los beneficios de un usuario premium.
+        </p>
         <p class="pa-dos">Ahorra tiempo y dinero con nuestros planes gratuitos.</p>
-          <p class="pa-tres">Conoce mas sobre los beneficios de ser miembro.</p>
-          <p class="pa-cuatro">Registrate Ya...!!!</p>
+        <p class="pa-tres">Conoce mas sobre los beneficios de ser miembro.</p>
+        <p class="pa-cuatro">Registrate Ya...!!!</p>
 
     </section>
 
 
-<footer>
+    <footer>
 
-</footer>
- </body>
+    </footer>
+</body>
+
 </html>
